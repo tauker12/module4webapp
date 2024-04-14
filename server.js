1b12f4e4f5f1b7ecbce2fa10548471ad3344d97c
tauker12 <164672973+tauker12@users.noreply.github.com>
2024-04-14 14:54:09 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const http  = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    // set header content type 
+    res.setHeader('Content-Type', 'text/html');
+
+    //send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            read.end();
+        } else {
+            //res.write(data);
+            res.end(data);
+        }
+    })
+
+});
+
+server.listen(3000, 'localhost' , () => {
+    console.log('Listening for requests on port 3000');
+});
